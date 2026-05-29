0374569470795ef1b373b5ae2b517a45feae03d4
Vanshika-OFFICIAL <vanshika9354@gamil.com>
2026-05-29 20:10:16 +0530

    Fix noteRoutes imports

--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,20 +1,17 @@
 const router = require("express").Router();
 
-const auth = require("./middleware/auth");
+const auth = require("../middleware/auth");
 
 const {
   createNote,
   getNotes,
   updateNote,
   deleteNote,
-} = require("./controllers/noteController");
+} = require("../controllers/noteController");
 
 router.post("/", auth, createNote);
-
 router.get("/", auth, getNotes);
-
 router.put("/:id", auth, updateNote);
-
 router.delete("/:id", auth, deleteNote);
 
 module.exports = router;
